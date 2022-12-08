591f4181dec81a8bd370dfd67709385034d1f2cb
Awlad Hossen <mdawladhossenmito199922@gmail.com>
2022-12-08 18:26:40 +0600

    Action types and Action Creators

--- src/components/page/Home.js
+++ src/components/page/Home.js
@@ -26,7 +26,7 @@ console.log(state)
       <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 m-5 justify-center">
       {
   products.map((product)=>(
-    <ProductsCard product={product}/>
+    <ProductsCard key={product.id} product={product}/>
   ))
 }
       </div>
--- src/components/page/ProductsCard.js
+++ src/components/page/ProductsCard.js
@@ -1,16 +1,21 @@
 import React from 'react'
+import { useDispatch } from 'react-redux';
+import { ADD_TO_CART } from '../../redux/actionTypes/actionTypes';
+import { addToCart } from '../../redux/productActions/productActions';
 
 export const ProductsCard = ({product}) => {
-    const { name, img, price, description,} = product;
+    const { name, img, price, description,id} = product;
+    const dispatch = useDispatch()
   return (
     <>
     <div className="card  bg-base-100 shadow-xl">
   <figure><img src={img} alt="Shoes" /></figure>
   <div className="card-body">
     <h2 className="card-title my-3">NAME : {name}</h2>
+    <h2 className="card-title my-3">PRICE : {price}</h2>
     <p>{description}</p>
     <div className="card-actions justify-end my-3">
-      <button className="btn btn-primary">Buy Now</button>
+      <button onClick={()=> dispatch(addToCart(product))} className="btn btn-primary">ADD TO CARAT</button>
     </div>
   </div>
 </div>
