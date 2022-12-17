7ade7fc9f8c97d8179fa36fc0f7df689dff5da66
Awlad Hossen <mdawladhossenmito199922@gmail.com>
2022-12-18 01:47:10 +0600

    Handling complex logic inside reducer. (part-3)

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,6 +3,6 @@ import {createStore} from "redux"
 import productReducer from "./reducer/productReducer"
 
 
-const  store = createStore(productReducer,composeWithDevTools())
+const  store = createStore(productReducer,composeWithDevTools())//google search using redux devtool
 
 export default store;
